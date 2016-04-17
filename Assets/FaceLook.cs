53060b476db2ba6a59b255483ac361ceaa1717b0
Greg <gbatha@gmail.com>
2016-04-16 20:09:39 -0700

    Adjusted non-solid form controls. Added face looking

---
+++ Assets/FaceLook.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FaceLook : MonoBehaviour {
+	public float radius = 0.2f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"));
+		Vector3 pos = transform.localPosition;
+		pos.x = offset.x * radius;
+		pos.y = offset.y * radius;
+		transform.localPosition = pos;
+	}
+}
