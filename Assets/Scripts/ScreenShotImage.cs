17004dd51a1a925ba385310f342e32e01d629c1c
Greg <gbatha@gmail.com>
2016-05-03 01:49:47 -0700

    Takin some screenshots

---
+++ Assets/Scripts/ScreenShotImage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShotImage : MonoBehaviour {
+	[SerializeField]
+	int superSize = 1;
+	[SerializeField]
+	string key = "s";
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown(key)){
+			string now = System.DateTime.Now.ToString("MM-dd-yyyy H.mm.ss");
+			string filename = "ScreenshotFolder/"+now+".png";
+			Application.CaptureScreenshot(filename, superSize);
+			Debug.Log("screenshot saved as "+filename);
+		}
+	}
+}
